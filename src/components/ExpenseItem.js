5519a5f3ddf47211c2b0f0f496be52b2ab24cfcd
MarioBravoP <mariobravopineda@gmail.com>
2024-04-22 09:29:36 -0400

    Completed final project Lab

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,27 @@ import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+    const { dispatch, currency } = useContext(AppContext);
     const handleDeleteExpense = () => {
         dispatch({
             type: 'DELETE_EXPENSE',
             payload: props.id,
         });
     };
+
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    };
+
     const increaseAllocation = (name) => {
         const expense = {
             name: name,
@@ -22,8 +36,9 @@ const ExpenseItem = (props) => {
     return (
         <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td>{currency}{props.cost}</td>
+        <td><button className='btn btn-success rounded-circle' onClick={event=> increaseAllocation(props.name)} style= {{color:'white', textAlign:'center'}}>+</button></td>
+        <td><button className='btn btn-danger rounded-circle' onClick={event=> decreaseAllocation(props.name)} style= {{color:'white', textAlign:'center'}}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
